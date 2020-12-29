c4d781a6cf22b22822a5a4e124aa88bf3ea7b354
Andres Felipe Zuluaga <andresfelipezuluaga@MacBook-Pro-de-Wolox.local>
2020-12-29 10:05:57 -0500

    Add react-navigation.

--- wbooks/src/app/components/BookList/index.tsx
+++ wbooks/src/app/components/BookList/index.tsx
@@ -3,11 +3,28 @@ import { FlatList, ListRenderItem } from 'react-native';
 import ItemBook from '@components/ItemBook';
 import { BOOKS_MOCK } from '@constants/mockBooks';
 import { Book } from '@interfaces/Book';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
-function BookList() {
-  const renderItem: ListRenderItem<Book> = ({ item }: { item: Book }) => <ItemBook {...item} />;
+type RootStackParamList = {
+  BookList: undefined;
+  BookDetails: undefined;
+};
+
+type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'BookList'>;
+
+type Props = {
+  navigation: ProfileScreenNavigationProp;
+};
+
+function BookList(navigation: Props) {
+  const renderItem: ListRenderItem<Book> = ({ item }: { item: Book }) => (
+    <TouchableOpacity onPress={() => navigation.navigation.navigate('BookDetails')}>
+      <ItemBook {...item} />
+    </TouchableOpacity>
+  );
   const keyExtractor = (item: Book) => `${item.id}`;
   return (
     <>
---
+++ wbooks/src/app/screens/BookDetails/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BookDetails() {
+  return <></>;
+}
+
+export default BookDetails;
