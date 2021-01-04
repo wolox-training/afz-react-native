096426a3c9072775ee079aab8cb6a1792a84894f
Andres Felipe Zuluaga <andresfelipezuluaga@MacBook-Pro-de-Wolox.local>
2021-01-04 13:42:38 -0500

    Deleted Images.ts and add import image files

--- wbooks/src/app/components/Header/index.tsx
+++ wbooks/src/app/components/Header/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Image, ImageBackground, Text, TouchableOpacity } from 'react-native';
 import { Header } from '@interfaces/Header';
 import { useNavigation } from '@react-navigation/native';
-import Images from '@constants/Images';
+import navBar from '@assets/bc_nav_bar.png';
+import icBack from '@assets/ic_back.png';
 
 import styles from './styles';
 
@@ -10,9 +11,9 @@ function HeaderContainer({ title }: Header) {
   const navigation = useNavigation();
   const back = () => navigation.goBack();
   return (
-    <ImageBackground source={Images.navBar} style={styles.headerImage}>
+    <ImageBackground source={navBar} style={styles.headerImage}>
       <TouchableOpacity onPress={back}>
-        <Image source={Images.back} style={styles.buttonBack} />
+        <Image source={icBack} style={styles.buttonBack} />
       </TouchableOpacity>
       <Text style={styles.titleHeader}>{title}</Text>
     </ImageBackground>
--- wbooks/src/constants/Images.ts
+++
@@ -1,6 +0,0 @@
-const Images = {
-  navBar: require('@assets/bc_nav_bar.png'),
-  back: require('@assets/ic_back.png')
-};
-
-export default Images;
