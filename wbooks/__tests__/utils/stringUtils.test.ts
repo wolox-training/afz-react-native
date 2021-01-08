04ba2608731946376957584cccc53dfaffe2c696
Andres Felipe Zuluaga <andresfelipezuluaga@MacBook-Pro-de-Wolox.local>
2021-01-08 08:19:49 -0500

    Edited stringUtils.ts

--- wbooks/__tests__/utils/stringUtils.test.ts
+++ wbooks/__tests__/utils/stringUtils.test.ts
@@ -1,35 +1,35 @@
 import { getInitials, keepNumbers } from '../../src/utils/stringUtils';
 
 describe('Test getInitials', () => {
-  test('Initials of Wolox is: W', () => {
+  it('Initials of Wolox is: W', () => {
     expect(getInitials('Wolox')).toBe('W');
   });
-  test('Text is empty', () => {
+  it('Text is empty', () => {
     expect(getInitials('')).toBe('');
   });
 
   test('Text is undefined', () => {
-    expect(getInitials(undefined)).toBe(undefined);
+    expect(getInitials(undefined)).toBeUndefined();
   });
 });
 
 describe('Test keepNumber', () => {
-  test('Numbers in text: 44', () => {
+  it('Numbers in text: 44', () => {
     expect(keepNumbers('C4S4')).toBe('44');
   });
 
-  test('All is numbers', () => {
+  it('All is numbers', () => {
     expect(keepNumbers('0987654321')).toBe('0987654321');
   });
 
-  test('Return only integer numbers', () => {
+  it('Return only integer numbers', () => {
     expect(keepNumbers(1.5)).toBe('15');
   });
 
-  test('Return string', () => {
+  it('Return string', () => {
     expect(keepNumbers(30)).toBe('30');
   });
-  test('Return empty', () => {
-    expect(keepNumbers(undefined)).toBe('');
+  it('Return empty', () => {
+    expect(keepNumbers(undefined)).not.toBeUndefined();
   });
 });
