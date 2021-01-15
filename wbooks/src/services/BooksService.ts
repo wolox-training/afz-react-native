bc14a4fa498e106cb1e9d1f08901edc38ec286b9
Andres Felipe Zuluaga <andresfelipezuluaga@MacBook-Pro-de-Wolox.local>
2021-01-15 08:32:12 -0500

    Edited BookServices.ts

--- wbooks/src/services/BooksService.ts
+++ wbooks/src/services/BooksService.ts
@@ -1,13 +1,13 @@
 import api from '@config/api';
 import { ApiResponse, ApiErrorResponse, CLIENT_ERROR } from 'apisauce';
 
-export const login = (email: any, password: any) => {
+export const login = (email: string, password: string) => {
   return api.post('/auth/sign_in', { email, password });
 };
 
 export const bookService = async () => {
   try {
-    const user = login('ignacio.coluccio@wolox.com.ar', 'wolox1189');
+    const user = await login('ignacio.coluccio@wolox.com.ar', 'wolox1189');
     return Promise.resolve({
       ok: true,
       data: (await api.get('/books', (await user).headers)).data
