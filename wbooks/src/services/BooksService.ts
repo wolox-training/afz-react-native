b3b047455c58f218d3265d3e6ba748a52d72ab51
Andres Felipe Zuluaga <andresfelipezuluaga@MacBook-Pro-de-Wolox.local>
2021-01-15 15:02:59 -0500

    Edited BooksService.ts

--- wbooks/src/services/BooksService.ts
+++ wbooks/src/services/BooksService.ts
@@ -1,8 +1,6 @@
 import api from '@config/api';
 
-export const login = (credentials: { email: string; password: string }) => {
-  const { email, password } = credentials;
-  return api.post('/auth/sign_in', { email, password });
-};
+export const login = ({ email, password }: { email: string; password: string }) =>
+  api.post('/auth/sign_in', { email, password });
 
 export const bookService = (headers: any) => api.get('/books', headers);
